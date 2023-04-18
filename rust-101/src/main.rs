7f4d2885f53db2d36370a2c66e004d31b751b367
anupamthe99 <puspaanupam@gmail.com>
2023-04-19 02:28:14 +0545

    rust 101 variable

---
+++ rust-101/src/main.rs
@@ -0,0 +1,9 @@
+#![deny(clippy::all)]
+
+fn main() {
+    // immuatable variable
+    let name="anupam";
+    let mut address="Nepal";
+    address="Nepal,Asia";
+    println!("Hello, world! from Nepal and my name is ,{}",address);
+}
